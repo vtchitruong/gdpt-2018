a84dd79a573ae1859facfdb6823326f1531c4006
Mr School <vtchitruong@gmail.com>
2026-01-27 19:14:07 +0700

    Modify

--- grade-11/topic-f2/building-a-database-in-postgresql-part-2.sql
+++ grade-11/topic-f2/building-a-database-in-postgresql-part-2.sql
@@ -11,9 +11,9 @@ create table students (
 	birth_place varchar(50)
 );
 
--- Thêm dữ liệu vào bảng students
+-- Thêm mẫu tin vào bảng students
 insert into students(student_id, last_name, first_name, gender, birth_date, birth_place) 
 values
-	('220001', 'Tào', 'Tháo', 0, '2007-01-15', 'Osaka, Japan'),
-	('230001', 'Lưu', 'Bị', 0, '2008-01-14', 'Istanbul, Turkey'),
-	('240021', 'Tôn', 'Quyền', 0, '2009-02-18', 'Dublin, Ireland');
+	('221001', 'Tào', 'Tháo', 0, '2007-01-15', 'Osaka, Japan'),
+	('231001', 'Lưu', 'Bị', 0, '2008-01-14', 'Istanbul, Turkey'),
+	('241021', 'Tôn', 'Quyền', 0, '2009-02-18', 'Dublin, Ireland');
